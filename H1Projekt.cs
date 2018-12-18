b9314e491319ef6ff2a5dd7a952c760be92b0328
Unknown <alexander.bondgaard@live.dk>
2018-12-18 11:56:24 +0100

    Added project

---
+++ H1Projekt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MitProjektH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You and your family has decided that you wanna go on a roadtrip \n" +
+                "but before you go, you have to make a few choices. \n" +
+                "Your family consists of you, your wife and your two kids at the age of 10 \n" +
+                "\n" +
+                "You have to choose between 3 cars. Each with their own advantages and disadvantages \n" +
+                "\n" +
+                "Car 1: Kia Ceed stationcar \n" +
+                "+ Lots of space for packing \n" +
+                "+ Decent comfort \n" +
+                "+ Good km/l \n" +
+                "- Slow acceleration \n" +
+                "- Poor topspeed \n" +
+                "\n" +
+                "Car 2: Porsche 911 4 seater \n" +
+                "+ Fast acceleration \n" +
+                "+ High topspeed \n" +
+                "+ Great comfort in the front \n" + 
+                "- Very bad comfort in the back \n" +
+                "- Poor km/l \n" +
+                "- Poor packing space \n" +
+                "\n" +
+                "Car 3: Tesla Model X \n" +
+                "+ Fast acceleration \n" +
+                "+ Decent topspeed \n" + 
+                "+ Great comfort front and back \n" +
+                "+ Great packing space \n" +
+                "+ Minimal noise \n" +
+                "- Charging takes a long time \n" +
+                "- Few places to charge \n" +
+                "- If you run out of power it can be tough to get access to a chargingstation");
+            Console.ReadKey();
+        }
+    }
+    class Kia
+    {
+
+    }
+    class Porsche
+    {
+
+    }
+    class Tesla
+    {
+
+    }
+}
+
